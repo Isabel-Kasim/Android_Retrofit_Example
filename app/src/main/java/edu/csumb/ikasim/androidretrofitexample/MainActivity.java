2dee6ef4cdd05a39e9183d37d0be4054a15f9be0
ikasim <54857733+Isabel-Kasim@users.noreply.github.com>
2022-02-11 21:22:02 -0800

    updated xml files, added booksearches

--- app/src/main/java/edu/csumb/ikasim/androidretrofitexample/MainActivity.java
+++ app/src/main/java/edu/csumb/ikasim/androidretrofitexample/MainActivity.java
@@ -2,7 +2,11 @@ package edu.csumb.ikasim.androidretrofitexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button search_button = findViewById(R.id.searchButton);
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(newIntent(MainActivity.this));
+            }
+        });
+
     }
+    public static Intent newIntent(Context packageContext) {
+        Intent intent = new Intent(packageContext, BookSearchFragment.class);
+        return intent;
+    }
+
+
+
+
 }
